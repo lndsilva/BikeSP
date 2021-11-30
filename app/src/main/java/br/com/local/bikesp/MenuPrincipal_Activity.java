8ff1b2d60b8ae14f4c2ad3e128889dd242524e41
lndsilva <lndsilva@hotmail.com>
2021-11-30 09:04:53 -0300

    primeiro commit

---
+++ app/src/main/java/br/com/local/bikesp/MenuPrincipal_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.bikesp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPrincipal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_principal_layout);
+    }
+}
